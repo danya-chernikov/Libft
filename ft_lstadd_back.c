8a90d1a9a675e52a221e445d4aceb93344c13cbb
danya-chernikov <dchernik@proton.me>
2024-12-27 16:56:09 +0100

    42 Header were added to pass the norm

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchernik <dchernik@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 16:55:01 by dchernik          #+#    #+#             */
+/*   Updated: 2024/12/27 16:55:53 by dchernik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*nptr;
-	
+
 	nptr = (*lst)->next;
 	while (nptr->next != NULL)
 		nptr = nptr->next;

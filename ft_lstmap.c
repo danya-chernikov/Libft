fec4471d1893221f5ec9445f79a937259fc9172e
Daniil Chernikov <dchernik@c3r6s6.42urduliz.com>
2025-02-05 17:49:51 +0100

    The ft_lstmap() function was reduced

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,4 +1,7 @@
 #include "libft.h"
+#include <stdlib.h>
+
+static int	alg(t_list *new_list_head, t_list *node, t_list *nptr);
 
 /* This function must use del() in case of a memory allocation error when
  * creating a new list! That's what the del() function is for! It is
@@ -17,6 +20,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (new_list_head == NULL)
 		return (NULL);
 	f(new_list_head->content);
+	if (!alg(new_list_head, node, nptr))
+		return (NULL);
+	return (new_list_head);
+}
+
+static int	alg(t_list *new_list_head, t_list *node, t_list *nptr)
+{
 	nptr = nptr->next;
 	while (nptr != NULL)
 	{
@@ -30,11 +40,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 				free(nptr);
 				nptr = nptr->next;
 			}
-			return (NULL);
+			return (0);
 		}
-		ft_lstadd_back(new_list_head, node);
+		ft_lstadd_back(&new_list_head, node);
 		f(node->content);
 		nptr = nptr->next;
 	}
-	return (new_list_head);
+	return (1);
 }
